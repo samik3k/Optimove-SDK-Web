4f23626305de428e6dfd8789ce2adfff117023cb
Konstantin Antipochkin <konstantin_a@optimove.com>
2023-02-01 14:50:53 -0500

    Removed src from package json files

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,56 @@
-import Optimove from './optimove';
+import { optimoveSDK } from './sdk';
 
-export default Optimove;
+export default class Optimove {
+  private token: string;
+  private initialized: boolean;
+
+  constructor(token: string) {
+    this.token = token;
+    this.initialized = false;
+  }
+
+  private async ensureInitialization(): Promise<void> {
+    if (this.initialized) {
+      return;
+    }
+
+    this.initialized = true;
+    await new Promise<void>((resolve) => {
+      optimoveSDK.initialize(this.token, null, resolve, null);
+    });
+  }
+
+  async registerUser(userId: string, email: string): Promise<void> {
+    await this.ensureInitialization();
+    optimoveSDK.API.registerUser(userId, email);
+  }
+
+  async setUserId(userId: string): Promise<void> {
+    await this.ensureInitialization();
+    optimoveSDK.API.setUserId(userId);
+  }
+
+  async setEmail(email: string): Promise<void> {
+    await this.ensureInitialization();
+    optimoveSDK.API.setUserEmail(email);
+  }
+
+  async setPageVisit(
+    url: string = location.href,
+    pageTitle: string = document.title,
+    pageCategory?: string,
+  ): Promise<void> {
+    await this.ensureInitialization();
+
+    optimoveSDK.API.setPageVisit(url, pageTitle, pageCategory);
+  }
+
+  async reportEvent(
+    event: string,
+    params: { [k: string]: string | number | boolean } = {},
+  ): Promise<void> {
+    await this.ensureInitialization();
+
+    optimoveSDK.API.reportEvent(event, params);
+  }
+}
